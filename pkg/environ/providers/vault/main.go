f14375450fa0577f6f648962852053e462a83178
albert <albert@dixon.rocks>
2019-05-08 13:50:35 -0700

    Be consistent about passing non-pointers and more debug

--- pkg/environ/providers/vault/main.go
+++ pkg/environ/providers/vault/main.go
@@ -79,6 +79,7 @@ func New() (environ.Provider, error) {
 	if er := env.ParseWithFuncs(v, p); er != nil {
 		return nil, er
 	}
+	log.Debugf("Generated new vault client. client=%+v", v)
 
 	if v.Token() == "" {
 		if er := v.SetVaultToken(); er != nil {
@@ -191,14 +192,14 @@ func (client *Client) AddToEnviron(env *environ.Environ) error {
 
 	for _, key := range client.Keys {
 		wg.Add(1)
-		go func(k *KVKey) {
+		go func(k KVKey) {
 			defer wg.Done()
 			if data, er := client.getKVData(k); er == nil {
 				env.SafeMerge(data)
 			} else {
 				log.Debugf("Failed to get data for key. key=%s err=%v", k.Path, er)
 			}
-		}(&key)
+		}(key)
 	}
 
 	if !util.IsBlank(client.AwsRole) || !util.IsBlank(client.IamRole) {
@@ -266,7 +267,7 @@ func (client *Client) AddToEnviron(env *environ.Environ) error {
 	return nil
 }
 
-func (client *Client) getKVData(key *KVKey) (map[string]string, error) {
+func (client *Client) getKVData(key KVKey) (map[string]string, error) {
 	keyParts := strings.Split(key.Path, "/")
 	if len(keyParts) < 2 {
 		return nil, ErrInvalidKVKey
